9c1e67b9d076dd458ff6930883f201c2836bad51
IK-Bruno97 <69594989+IK-Bruno97@users.noreply.github.com>
2022-12-19 16:14:55 +0100

    Update settings.py

--- brunodelivery/settings.py
+++ brunodelivery/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://web-production-47e2.up.railway.app/']
 
 
 # Application definition
